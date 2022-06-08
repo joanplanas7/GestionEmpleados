36ed28bd079b2629455e48c0f14c925461f126ec
joanplanas7 <72151839+joanplanas7@users.noreply.github.com>
2022-06-08 16:48:23 +0200

    vista welcome terinada

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,8 +14,8 @@
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-2">
+            <div class="collapse navbar-collapse flex-row-reverse" id="navbarSupportedContent">
+                <ul class="nav navbar-nav ml-auto">
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="./login">Login</a>
                     </li>
@@ -27,6 +27,6 @@
         </div>
     </nav>
     <div class="container">
-        <h1 class="titlePag">Login or registery</h1>
+        <h1 class="titlePag">Login or register</h1>
     </div>
 @endsection 
